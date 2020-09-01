c2318cd0b03571d7b6a8aa42d0e424ddee593a0d
lucas.castro@acct.global <lucas.sc98@hotmail.com>
2020-09-01 20:27:55 -0300

    Starting lesson 9

---
+++ eventos/src/view/usuario-novo/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./usuario-novo.css";
+
+import firebase from "../../config/firebase";
+import "firebase/auth";
+
+function NovoUsuario(){
+    return (
+        <h1>Novo usuário</h1>
+    )
+}
+
+export default NovoUsuario;
