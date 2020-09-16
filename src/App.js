759c5efea5ceea24cc3db4a0bfb56895b5e01faa
Lucas Castro <lucas.sc98@hotmail.com>
2020-09-15 21:12:17 -0300

    Finish application applying logged localStorage information

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import store from "./store/";
-import { Provider } from 'react-redux';
+import { store, persistor } from "./store/";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 /*Paginas*/
 import Login from "./view/login/";
@@ -14,16 +15,22 @@ import EventoDetalhes from "./view/evento-detalhes/";
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route path="/eventos/:parametro" component={Home} />
-        <Route exact path="/novo-usuario" component={NovoUsuario} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/recuperar-senha" component={UsuarioRecuperarSenha} />
-        <Route exact path="/cadastrar-evento" component={EventoCadastro} />
-        <Route path="/detalhes-do-evento/:id" component={EventoDetalhes} />
-        <Route path="/editar-evento/:id" component={EventoCadastro} />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route path="/eventos/:parametro" component={Home} />
+          <Route exact path="/novo-usuario" component={NovoUsuario} />
+          <Route exact path="/login" component={Login} />
+          <Route
+            exact
+            path="/recuperar-senha"
+            component={UsuarioRecuperarSenha}
+          />
+          <Route exact path="/cadastrar-evento" component={EventoCadastro} />
+          <Route path="/detalhes-do-evento/:id" component={EventoDetalhes} />
+          <Route path="/editar-evento/:id" component={EventoCadastro} />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
